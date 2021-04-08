ed5f5f128cb3a2234ec52aa828e30db9288f858e
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-08 23:50:28 +0200

    aggiunto una Local Reference per utilizzare il binding in angular

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -8,17 +8,23 @@ import {Component, OnInit,EventEmitter,Output} from '@angular/core';
 export class ChildComponentComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output()  blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer(){
-    this.serverCreated.emit({serverName: this.newServerName, serverContent:this.newServerContent});
+  onAddServer(serverInput){
+    this.serverCreated.emit({
+      serverName: serverInput.value,
+      serverContent:this.newServerContent
+    });
   }
-  onAddBlueprint(){
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent:this.newServerContent});
+  onAddBlueprint(serverInput){
+    this.blueprintCreated.emit({
+      serverName: serverInput.value,
+      serverContent:this.newServerContent
+    });
   }
 }
