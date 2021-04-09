97e2dccd8e4cbc72d176b74aec707b924254c557
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-09 21:11:34 +0200

    usatop ViewChild & ContentChild

--- src/app/child-two-component/child-two-component.component.ts
+++ src/app/child-two-component/child-two-component.component.ts
@@ -1,18 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-child-two-component',
   templateUrl: './child-two-component.component.html',
   styleUrls: ['./child-two-component.component.css']
 })
-export class ChildTwoComponentComponent implements OnInit {
+export class ChildTwoComponentComponent implements OnInit, OnChanges,DoCheck,AfterContentInit {
 
   @Input('alias-quellochevoglio') element : {type: string, name: string, content:string }; /* dichiariamo l'elemento con typescript */
+  @ContentChild('paragraphName') static: true;
 
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor call')
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges call')
+    console.log(changes)
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit call')
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck call')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit call')
+  }
 }
